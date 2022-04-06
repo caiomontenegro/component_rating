4e53c0636fc6c4725f4ffe1876fb66ea14dbb84a
Caio Montenegro <caio.almeida@portaldotrader.com.br>
2022-04-06 20:37:46 -0300

    Value grade style fixed

--- components/01-Interative-rating/script/script.js
+++ components/01-Interative-rating/script/script.js
@@ -1,10 +1,28 @@
 let evaluation = null
+let selected = false
+let number = null
 
 
 function grading(rating) {
+    if(selected === false ) {
+        number = rating
+    }
+    selected = true
+    console.log(number)
     let confirm = document.querySelector('div.confirm-js')
+    let button = document.getElementById(rating)
     evaluation = rating
     confirm.innerHTML = `You selected ${evaluation} out of 5`
+    button.style.backgroundColor = '#fb7413'
+    button.style.color = 'white'
+    if (number !== evaluation) {
+        let old = document.getElementById(number)
+        old.style.backgroundColor = '#344250'
+        old.style.color = '#7c8798'
+        console.log('desliga o anterior')
+        number = rating
+        console.log(number)
+    }
 }
 
 function confirm() {
